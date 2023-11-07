867c8897a613710f4b75f71f19291b5b2b5b2590
manvi <manvi@Manvitha>
2023-11-07 17:17:48 +0530

    actual comit

---
+++ src/com/practice/GoodMorning.java
@@ -0,0 +1,8 @@
+package com.practice;
+
+public class GoodMorning {
+void morning()
+{
+	System.out.println("Good Morning Master..");
+}
+}
---
+++ src/com/practice/GoodNight.java
@@ -0,0 +1,8 @@
+package com.practice;
+
+public class GoodNight {
+	void night()
+	{
+		System.out.println("Good Night Master..");
+	}
+}
